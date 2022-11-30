aa56db251dacef19c2a0e8f2e22c725b108a334f
kavitadsharma <kavitadsharma899107@gmail.com>
2022-11-30 10:56:23 +0530

    check the correction

--- pelindrome.js
+++ pelindrome.js
@@ -2,7 +2,7 @@ function checkPalindrome(N, str) {
     let bag="";
     for(let i=str.length-1;i>=0;i--){
         bag=bag+str[i]
-    }if(str==0){
+    }if(str==bag){
         console.log("Yes")
     }else{
         console.log("No")
